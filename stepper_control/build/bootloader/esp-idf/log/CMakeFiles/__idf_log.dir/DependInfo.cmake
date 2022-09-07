07a5adc7ae58a75a12474972a45c639a122a758b
Mahesh Tupe <tupemahesh91@gmail.com>
2022-09-07 09:44:53 +0530

    Added initial code for acceleration control of stepper motor

---
+++ stepper_control/build/bootloader/esp-idf/log/CMakeFiles/__idf_log.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/mahesh/esp/esp-idf/components/log/log.c" "/home/mahesh/Inverse_Pendulum/stepper_control/build/bootloader/esp-idf/log/CMakeFiles/__idf_log.dir/log.c.obj"
+  "/home/mahesh/esp/esp-idf/components/log/log_buffers.c" "/home/mahesh/Inverse_Pendulum/stepper_control/build/bootloader/esp-idf/log/CMakeFiles/__idf_log.dir/log_buffers.c.obj"
+  "/home/mahesh/esp/esp-idf/components/log/log_noos.c" "/home/mahesh/Inverse_Pendulum/stepper_control/build/bootloader/esp-idf/log/CMakeFiles/__idf_log.dir/log_noos.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "config"
+  "/home/mahesh/esp/esp-idf/components/log/include"
+  "/home/mahesh/esp/esp-idf/components/esp_rom/include"
+  "/home/mahesh/esp/esp-idf/components/esp_rom/esp32"
+  "/home/mahesh/esp/esp-idf/components/esp_common/include"
+  "/home/mahesh/esp/esp-idf/components/xtensa/include"
+  "/home/mahesh/esp/esp-idf/components/xtensa/esp32/include"
+  "/home/mahesh/esp/esp-idf/components/esp32/include"
+  "/home/mahesh/esp/esp-idf/components/soc/src/esp32/."
+  "/home/mahesh/esp/esp-idf/components/soc/src/esp32/include"
+  "/home/mahesh/esp/esp-idf/components/soc/include"
+  "/home/mahesh/esp/esp-idf/components/soc/soc/esp32/include"
+  "/home/mahesh/esp/esp-idf/components/soc/soc/esp32/../include"
+  "/home/mahesh/esp/esp-idf/components/soc/soc/esp32/private_include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/mahesh/Inverse_Pendulum/stepper_control/build/bootloader/esp-idf/esp_rom/CMakeFiles/__idf_esp_rom.dir/DependInfo.cmake"
+  "/home/mahesh/Inverse_Pendulum/stepper_control/build/bootloader/esp-idf/soc/CMakeFiles/__idf_soc.dir/DependInfo.cmake"
+  "/home/mahesh/Inverse_Pendulum/stepper_control/build/bootloader/esp-idf/efuse/CMakeFiles/__idf_efuse.dir/DependInfo.cmake"
+  "/home/mahesh/Inverse_Pendulum/stepper_control/build/bootloader/esp-idf/soc/soc/esp32/CMakeFiles/soc_esp32.dir/DependInfo.cmake"
+  "/home/mahesh/Inverse_Pendulum/stepper_control/build/bootloader/esp-idf/bootloader_support/CMakeFiles/__idf_bootloader_support.dir/DependInfo.cmake"
+  "/home/mahesh/Inverse_Pendulum/stepper_control/build/bootloader/esp-idf/spi_flash/CMakeFiles/__idf_spi_flash.dir/DependInfo.cmake"
+  "/home/mahesh/Inverse_Pendulum/stepper_control/build/bootloader/esp-idf/micro-ecc/CMakeFiles/__idf_micro-ecc.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
