0c7bac6656729c02aa76b6e7c6563225bef6500f
vrnimje <vedantnimjed@gmail.com>
2022-09-18 12:14:26 +0530

    Added design pdf and alsoconsidered the actual system's values in octave

--- Octave_Tasks/Vedant_Task_1/Inverted_Pendulum.m
+++ Octave_Tasks/Vedant_Task_1/Inverted_Pendulum.m
@@ -5,20 +5,21 @@ pkg load image;
 function draw_cart_pendulum(y, L)
   theta = y(1);
   dist = y(3);
-  Len = 1;
-  B = 1;
+  #disp(dist);
+  Len = 15;
+  B = 15;
   x1 = dist - L*sin(pi-theta);      ## x-coordinate of pendulum bob
   y1 = L*cos(pi - theta);  ## y coordinate of pendulum bob
-  d = 0.3;               ## diameter of pendulum bob
+  d = 5;               ## diameter of pendulum bob
   hold on;
   clf;
   axis equal;
   rectangle('Position',[((dist)-(Len/2)),0,Len,B],'FaceColor',[0 0 1]);
   rectangle('Position',[(x1-(d/2)),(y1-(d/2)),d,d],'Curvature',1,'FaceColor',[1 0 0]);
   line([dist x1], [B/2 y1], "linestyle", "-", "color", "k");
-  line([-5 5], [0 0], "linestyle", "-", "color", "k");
-  xlim([-5 5])
-  ylim([-5 5])
+  line([-40 40], [0 0], "linestyle", "-", "color", "k");
+  xlim([-40 40])
+  ylim([-50 50])
   drawnow
   hold off
 endfunction
@@ -26,15 +27,17 @@ endfunction
 function dy = inverted_pendulum_dynamics(y, m, M, L, g, u)
   sin_theta = sin(y(1));
   cos_theta = cos(y(1));
+  f = 1;
   dy(1,1) = y(2);
   dy(3,1) = y(4);
-  dy(4,1) = (-(m*g*cos_theta*sin_theta) - (m*L*sin_theta*((y(2))^2)) + u)/(M + m*(sin_theta^2));
+  dy(4,1) = (-(m*g*cos_theta*sin_theta) + (m*L*sin_theta*((y(2))^2) - f*y(4)) + u)/(M + m*(sin_theta^2));
   dy(2,1) = (-(g*sin_theta) + (dy(4,1)*cos_theta))/L;
 
 endfunction
 
 function [A,B] = inverted_pendulum_AB_matrix(m, M, g, L)
-  A = [0 1 0 0; (g+(m*g/M))/L 0 0 0; 0 0 0 1; -m*g/M 0 0 0];
+  f = 0.8;
+  A = [0 1 0 0; (g+(m*g/M))/L 0 0 -f/(M*L); 0 0 0 1; -m*g/M 0 0 -f/M];
   B = [0; -1/(L*(M)); 0; 1/(M)];
 endfunction
 
@@ -43,8 +46,8 @@ function [t,y,K] = lqr_inverted_pendulum(m, M, g, L, y_setpoint, y0)
   sys = ss(A,B);
   sys_d = ss(c2d(sys, 0.05));
   #step(sys_d);
-  Q = [100 0 0 0; 0 100 0 0; 0 0 10 0; 0 0 0 10];                   ## Initialise Q matrix
-  R = 0.001;                   ## Initialise R
+  Q = [74.83148 0 0 0; 0 7.483148 0 0; 0 0 44500 0; 0 0 0 10];                   ## Initialise Q matrix
+  R = 0.015;                   ## Initialise R
   K = dlqr(sys_d,Q,R);  ## Calculate K matrix from A,B,Q,R matrices
 ##K = lqr(A,B,Q,R);
   tspan = 0:0.05:10;       ## Initialise time step
@@ -52,17 +55,18 @@ function [t,y,K] = lqr_inverted_pendulum(m, M, g, L, y_setpoint, y0)
 endfunction
 
 function inverted_pendulum_main()
-  m = 1;
-  M = 2;
-  g = 9.8;
-  L = 3;
-  y_setpoint = [pi; 0; 2; 0];                                  ## Set Point
-  y0 = [3*pi/4; 0; 1; 0];  ## Initial condtion
+  m = 240;
+  M = 500;
+  g = 980;
+  L = 49.5;
+  y_setpoint = [pi; 0; 0; 0];                                  ## Set Point
+  y0 = [2.82; 0; 40; 0];  ## Initial condtion
   [t,y,K] = lqr_inverted_pendulum(m, M, g, L, y_setpoint, y0);
   y_ = repmat(transpose(y_setpoint), 201, 1);
-  #disp(y_);
+  disp(K);
+  disp(y);
   u = -K*(transpose(y-y_));
-  plot(t,u)
+  disp(max(u/(M+m)));
   #disp([t,y]);
 ##  plot(t,y(:,1));
 ##  hold on
@@ -71,7 +75,7 @@ function inverted_pendulum_main()
 ##  hold on
 ##  plot(t,y(:,4),"--g");
 ##  hold off
-##  for k = 1:length(t)
-##    #draw_cart_pendulum(y(k, :), L);
-##  endfor
+  for k = 1:length(t)
+    draw_cart_pendulum(y(k, :), L);
+  endfor
 endfunction
